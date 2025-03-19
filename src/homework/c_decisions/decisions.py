b531a8c48624c4ec23ceb8d5b784c6cc2ed3f308
whittleman12 <whitman.howard@g.austincc.edu>
2025-03-19 17:55:18 +0000

    Homework 2 - whittleman12

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,16 @@
-#
+def get_options_ratio(options, total):
+    ratio = options / total
+    return float(ratio)
+
+def get_faculty_rating(ratio):
+    ratio
+    if .9 <= ratio <= 1:
+        return "Excellent"
+    elif ratio >= .8:
+        return "Very Good"
+    elif ratio >= .7:
+        return "Good"
+    elif ratio >= .6:
+        return "Needs Improvement"
+    else:
+        return "Unacceptable"
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,8 @@
-#
+import decisions
+
+options = int(input("Enter a score: "))
+total = int(input("Enter the maximum score: "))
+
+result = decisions.get_options_ratio(options, total)
+
+print(decisions.get_faculty_rating(result))
