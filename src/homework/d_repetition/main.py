1aa705e66329d426e5f44bf2711db5f096bcc076
whittleman12 <whitman.howard@g.austincc.edu>
2025-03-26 20:46:53 +0000

    Homework 3

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,34 @@
-#
+import repetition
+
+def menu():
+    print("""Homework 3 Menu
+    1-Factorial
+    2-Sum odd numbers
+    3-Exit""")
+
+def run_menu():
+    menu()
+    option = int(input("Select a menu option: "))
+    handle_menu_option(option)
+
+def handle_menu_option(option):
+    if option == 1:
+        num = int(input("Enter a number between 1 and 10: "))
+        if 0 < num < 10:
+            repetition.get_factorial(num) 
+        else:
+            print("You must enter a valid number")
+        print("Do you want to exit?")
+    elif option == 2:
+        num = int(input("Enter a number between 1 and 99: "))
+        if 0 < num < 100:
+            repetition.sum_odd_numbers(num)
+        else:
+            print("You must enter a valid number")
+        print("Do you want to exit?")
+    elif option == 3:
+        print("Do you want to exit?")
+    else:
+        print("Invalid option, please try again")
+
+run_menu()
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,15 @@
-#
+
+def get_factorial(num):
+    result = 1
+    for num in range(1, num + 1):
+        result *= num
+    return result
+
+def sum_odd_numbers(num):
+    result = 0
+    x = 1
+    while x <= num:
+        if x % 2 != 0:
+            result += x
+        x += 1
+    return result
