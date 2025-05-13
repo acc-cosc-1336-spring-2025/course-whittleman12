855e19e0820ac597f9f7ee6f411ed8b1a9135ff5
whittleman12 <whitman.howard@g.austincc.edu>
2025-05-13 17:34:33 +0000

    Homework 4

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,1 +1,36 @@
-#
+import strings
+
+def menu():
+    print("""
+1. Hamming Distance
+2. DNA Complement
+3. Exit
+          """)
+    
+def run_menu():
+    menu()
+    option = int(input("Select a menu option: "))
+    handle_menu(option)
+
+def handle_menu(option):
+    while True:
+        if option == 1:
+            print("The valid DNA characters are A, C, G, T.")
+            str1 = input("Enter a string of DNA: ")
+            str2 = input("Enter a second, different string of DNA: ")
+            print(f"The Hamming distance is {strings.get_hamming_distance(str1.upper(), str2.upper())}.")
+            
+        elif option == 2:
+            print("The valid DNA characters are A, C, G, T.")
+            dna = input("Enter a string of DNA: ")
+            print(f"The DNA complement is {strings.get_dna_complement(dna.upper())}.")
+            
+        elif option == 3:
+            print("You have chosen to exit.")
+            exit()
+        else:
+            print("Invalid choice.")
+            
+        run_menu()
+
+run_menu()
--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,22 @@
-#
+def get_hamming_distance(dna1, dna2):
+    if len(dna1) != len(dna2):
+        raise ValueError("The two strings must be of equal length.")
+    sum = 0
+    for ch in range(len(dna1)):
+        if dna1[ch] != dna2[ch]:
+            sum += 1
+    return sum
+
+
+
+def get_dna_complement(dna):
+    for ch in dna:
+        if ch == "A":
+            ch = "T"
+        if ch == "T":
+            ch = "A"
+        if ch == "G":
+            ch = "C"
+        if ch == "C":
+            ch ="G"
+    return dna[::-1]
